b258ff77bae591b261d3c49fb1205cbbdf9b02a6
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-21 00:20:54 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
   const watches=[
     {id: 1, name: 'CASIO', price: 500},
-    {id: 1, name: 'Apple', price: 3000},
+    {id: 1, name: 'Apple', price: 3300},
     {id: 1, name: 'Royal', price: 60000},
   ]
 
