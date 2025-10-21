84d359a7831f11896c567f0144dce38ef5927943
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-22 00:14:25 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Watch from './watch'
 function App() {
   
   const watches=[
-    {id: 1, name: 'CASIO', price: 500},
-    {id: 2, name: 'Apple', price: 3300},
+    {id: 1, name: 'CASIO watch', price: 500},
+    {id: 2, name: 'Apple watch', price: 3300},
     {id: 3, name: 'Royal Watch', price: 60000},
   ]
 
