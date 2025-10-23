fb77c520a53ec6bed23e642a5fb3ad43e4dd6214
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-23 18:48:12 +0600

    watches more data input just id, name, price

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import Watch from './watch'
 
 function App() {
   
-  const watches=[
-    {id: 1, name: 'CASIO watch', price: 500},
-    {id: 2, name: 'Apple watch', price: 3300},
-    {id: 3, name: 'Royal Watch', price: 60400},
-  ]
+  // const watches=[
+  //   {id: 1, name: 'CASIO watch', price: 500},
+  //   {id: 2, name: 'Apple watch', price: 3300},
+  //   {id: 3, name: 'Royal Watch', price: 60400},
+  // ]
+
+  const watches = [
+  { id: 1, name: "Apple Watch Series 9", price: 399 },
+  { id: 2, name: "Samsung Galaxy Watch 6", price: 349 },
+  { id: 3, name: "Fitbit Versa 4", price: 229 },
+  { id: 4, name: "Garmin Venu 3", price: 449 },
+  { id: 5, name: "Amazfit GTR 4", price: 199 }
+];
+
 
   return (
     <>
